34b4d228bf863824375f3b4a294302133a13c430
Dan Schellenberg <dan@Dans-iMac.local>
2018-03-21 16:58:21 -0600

    updated fizzArray2

--- data/array.js
+++ data/array.js
@@ -642,7 +642,7 @@ exercises.push(
     containing the numbers 0, 1, 2, ... n-1. The given n may be 0, in which case
     just return a length 0 array. You do not need a separate if-statement for the
     length-0 case; the for-loop should naturally execute 0 times in that case,
-    so it just works. The syntax to make a new array is let newArray = [];`
+    so it just works. The syntax to make a new array is let myArray = [];`
   },
   {
     title: 'Array-2',
@@ -681,9 +681,8 @@ exercises.push(
     ],
     question: `Given a number n, create and return a new string array of length n,
     containing the strings "0", "1" "2" .. through n-1. N may be 0, in which case
-    just return a length 0 array. Note: String.valueOf(xxx) will make the String
-    form of most types. The syntax to make a new string array is:
-    new String[desired_length]`
+    just return a length 0 array. Note: String(xxx) will make the String
+    form of most types.`
   },
   {
     title: 'Array-2',
