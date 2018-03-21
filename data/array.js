8cee73fe434ce1f78152349c5096f46af84b11c6
Dan Schellenberg <schellenberg@gmail.com>
2018-03-21 14:58:03 -0600

    fix fizzarray wording

--- data/array.js
+++ data/array.js
@@ -638,11 +638,11 @@ exercises.push(
       "(2)",
       "(7)",
     ],
-    question: `Given a number n, create and return a new int array of length n,
+    question: `Given a number n, create and return a new array of length n,
     containing the numbers 0, 1, 2, ... n-1. The given n may be 0, in which case
     just return a length 0 array. You do not need a separate if-statement for the
     length-0 case; the for-loop should naturally execute 0 times in that case,
-    so it just works. The syntax to make a new int array is: new int[desired_length]`
+    so it just works. The syntax to make a new array is let newArray = [];`
   },
   {
     title: 'Array-2',
