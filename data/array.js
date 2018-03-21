320d1b0e0459c6e58d871b2132ce8e195fdfeb82
Dan Schellenberg <schellenberg@gmail.com>
2018-03-21 15:15:21 -0600

    fixed java remnant from makeLast

--- data/array.js
+++ data/array.js
@@ -215,7 +215,7 @@ exercises.push(
     ],
     question: `Given an int array, return a new array with double the length where its last
     element is the same as the original array, and all the other elements are 0. The original
-    array will be length 1 or more. Note: by default, a new int array contains all 0's.`,
+    array will be length 1 or more.`,
   },
   {
     title: 'Array-1',
