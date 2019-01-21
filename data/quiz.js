cb50188ebfd906eb31d812e4cf276b3f87cd7a92
Dan Schellenberg <schellenberg@gmail.com>
2019-01-21 15:54:45 -0600

    Update quiz.js

--- data/quiz.js
+++ data/quiz.js
@@ -30,7 +30,24 @@ exercises.push(
       '(8, 8)',
       '(-16, 24)',
       '(9, 1)',
-    ], }
+    ], 
+  },
+    {
+      title: 'QuizQuestions',
+      name: 'countOdds',
+      inputs: [
+        "([2, 1, 2, 3, 4])",
+        "([2, 2, 0])",
+        "([1, 3, 5])",
+        "([])",
+        "([11, 9, 0, 1])",
+        "([2, 11, 9, 0])",
+        "([2])",
+        "([2, 5, 12])",
+      ],
+      question: `Return the number of odd integers in the given array. Note: the % "mod"
+      operator computes the remainder, e.g. 5 % 2 is 1.`
+    },
 )
 
 /** --- solutions --- **/
@@ -51,3 +68,14 @@ solutions.sumDownBy2 =  function (n) {
 solutions.makes8 = function (a, b) {
   return ((a == 8 || b == 8) || (a + b == 8));
 };
+
+solutions.countEvens = function (nums) {
+  let count = 0;
+
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i]%2 === 1){
+            count += 1;
+        }
+    }
+    return count;
+}
