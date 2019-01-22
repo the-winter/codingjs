8a24a6ff088f20787fb9361eba57e8c054fa6bce
Dan Schellenberg <schellenberg@gmail.com>
2019-01-22 11:35:06 -0600

    Update quiz.js

--- data/quiz.js
+++ data/quiz.js
@@ -50,7 +50,7 @@ exercises.push(
     },
     { question: 'The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition. The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence.',
       title: 'QuizQuestions',
-      name: 'fibonacciquiz',
+      name: 'fib',
       inputs: [
       "(0)",
       "(1)",
@@ -93,9 +93,9 @@ solutions.countOdds = function (nums) {
     return count;
 }
 
-solutions.fibonacciquiz = function (n) {
+solutions.fib = function (n) {
   if (n < 2) {
       return n;;
   };
-    return fibonacci(n-1) + fibonacci(n-2)
+    return fib(n-1) + fib(n-2)
 }
