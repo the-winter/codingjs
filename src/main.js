be5f5d11f9aef0aebd16380e07950ab7bbcdaa76
thewinter <19483860+the-winter@users.noreply.github.com>
2019-05-03 12:59:34 +0800

    let urls work in subdirs

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 let $ = require("jquery");
 
-if (location.pathname === "/") {
+
+var pathSuffix = location.pathname.split('/').splice(-1)[0]
+if (pathSuffix === "") {
   // main page
   require("./indexPage.js");
 }
 
-else if (location.pathname === "/exercise.html") {
+else if (pathSuffix === "exercise.html") {
   // individual problem/exercise page
   require("./exercisePage.js");
 }
@@ -13,6 +15,6 @@ else if (location.pathname === "/exercise.html") {
 // this lets you not display anything until the entire page is ready (since we set html
 //  to invisible in style.css)
 // useful, since otherwise you see the transition from textarea to codemirror, etc...
-$(document).ready(function() {
+$(document).ready(function () {
   document.getElementsByTagName("html")[0].style.visibility = "visible";
 });
